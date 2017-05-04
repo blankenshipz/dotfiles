b1f0c00c9494007b0577ddc121d1412a84ae03cd
Zach Blankenship <zach.blankenship@gmail.com>
2017-05-04 17:08:14 -0400

    Add some prompts to the setup script

--- setup.sh
+++ setup.sh
@@ -10,10 +10,13 @@ fi
 # Install brew-bundle
 brew tap Homebrew/bundle
 
+echo "Installing from Brewfile"
 # Install all the things! (see the Brewfile)
 brew bundle
 
 ## VIM
+
+echo "Setting up Vim"
 
 # make sure the backup folder exists
 mkdir -p $HOME/.vim/backup
@@ -24,3 +27,6 @@ mkdir -p $HOME/.vim/undo
 # Install Plug.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Please install prezto https://github.com/sorin-ionescu/prezto"
+
